1079cbfa86b659021c6da4c912dd1ffc82303fce
Ashish Gupta <59931129+Ashishgupta08@users.noreply.github.com>
2021-04-05 02:28:29 +0530

    added - images and list

--- javascripts/index.js
+++ javascripts/index.js
@@ -5,3 +5,20 @@ function openNav() {
 function closeNav() {
   document.getElementById("navBar").style.width = "0";
 }
+
+function openListContent() {
+  document.getElementById("list-content1").style.display = "block";
+  document.getElementById("arrowup").style.display = "block !important";
+  document.getElementById("arrowdown").style.display = "none";
+}
+
+function closeListContent() {
+  document.getElementById("list-content1").style.display = "none";
+  document.getElementById("arrowup").style.display = "none !important";
+  document.getElementById("arrowdown").style.display = "block";
+}
+
+function onLoad() {
+  document.getElementById("arrowup").style.display = "none";
+  document.getElementById("arrowdown").style.display = "block";
+}
